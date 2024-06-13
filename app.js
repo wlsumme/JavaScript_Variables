1f2384fe67f8bf6850ab7c5aa2bfa69e26ea4469
William Summe <wlsumme@gmail.com>
2024-06-13 14:13:21 -0500

    done

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
+const firstName = "William"
+lastName = "Summe";
+age = 36;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensative = true;
+
+// Exersise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 25;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss";
+let pageCount = 722; 
+let bookmark = 456;
+let hasREad = true;
